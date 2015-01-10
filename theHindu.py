5744b3bc285ff0de56675c9bfb19793a0e1c39ce
sarathm09 <sarathm09@gmail.com>
2015-01-10 16:48:37 +0530

    Speech Recognition +The  Hindu rss

--- theHindu.py
+++ theHindu.py
@@ -4,10 +4,15 @@ from threading import Thread
 
 
 def extractandsay(b):
+	print "do you want me to read the news?"
+	speech.say("do you want me to read the news?")
+	ans = speech.input()
+	sp = True if 's' in ans.lower() else False
 	for item in b.findAll('item'):
 		title = item.findAll('title')[0].text
 		print title
-		speech.say(title)
+		if sp:
+			speech.say(title + '.')
 		time.sleep(.5)
 
 
@@ -15,3 +20,4 @@ url = "http://www.thehindu.com/?service=rss"
 b = BeautifulSoup(requests.get(url).content)
 
 Thread(target=extractandsay, args=(b,)).start()
+
